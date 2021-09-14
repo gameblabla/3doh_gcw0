6da6cc8186378adc2de48c571928157ed224643a
gameblabla <gameblabla@protonmail.com>
2021-09-14 22:05:04 +0200

    Added CUE/IMG/BIN support.

--- src/cdrom.c
+++ src/cdrom.c
@@ -15,7 +15,55 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include "cuefile.h"
 FILE *fcdrom;
+
+static int cd_sector_size;
+static int cd_sector_offset;
+
+static void fsDetectCDFormat(const char *path)
+{
+   CD_format cd_format;
+   cueFile *cue_file = cue_get(path);
+   if (cue_file)
+   {
+      cd_format = cue_file->cd_format;
+      //printf("[4DO]: File format from cue file resolved to %s\n", cue_get_cd_format_name(cd_format));
+      free(cue_file);
+   }
+   else
+   {
+      int size = 0;
+      FILE *fp = fopen(path, "r");
+      if (fp) {
+         fseek(fp, 0L, SEEK_END);
+	     size = ftell(fp);
+	     fclose(fp);
+      }
+      cd_format = MODE1_2048;
+      if (size % SECTOR_SIZE_2352 == 0)
+      {
+    	 cd_format = MODE1_2352;
+      }
+   }
+
+   switch (cd_format)
+   {
+	   //case MODE1_2048:
+	   default:
+		  cd_sector_size = SECTOR_SIZE_2048;
+		  cd_sector_offset = SECTOR_OFFSET_MODE1_2048;
+		break;
+	   case MODE1_2352:
+		  cd_sector_size = SECTOR_SIZE_2352;
+		  cd_sector_offset = SECTOR_OFFSET_MODE1_2352;
+		break;
+	   case MODE2_2352:
+		  cd_sector_size = SECTOR_SIZE_2352;
+		  cd_sector_offset = SECTOR_OFFSET_MODE2_2352;
+		break;
+   }
+}
 
 int fsInit()
 {
@@ -46,11 +94,33 @@ void fsReadBios(char *biosFile, void *prom)
 
 int fsOpenIso(char *path)
 {
+	uint_fast8_t i;
+	char cue_path_base[256];
+	const char *exts[] = {".cue", ".CUE"};
+	const char *exts_bin[] = {".bin", ".BIN"};
+	
+	/* Detect if input file is .cue or .CUE and if so, change it to bin */
+	for(i=0;i<2;i++)
+	{
+		if (strcmp(strrchr(path, '.'), exts[i]) == 0)
+		{
+			strncpy(cue_path_base, path, 256);
+			char *last_dot = strrchr(cue_path_base, '.');
+			if (last_dot == NULL) return 0;
+			*(last_dot) = '\0';
+			strcpy(path, cue_path_base);
+			strcat(path, exts_bin[i]);
+			break;
+		}
+	}
+
 	fcdrom = fopen(path, "rb");
-	if (!fcdrom) {
+	if (!fcdrom)
+	{
 		printf("ERROR: can't load game file, exiting\n");
 		return 0;
 	}
+	fsDetectCDFormat(path);
 	return 1;
 }
 
@@ -66,8 +136,8 @@ int fsCloseIso()
 
 int fsReadBlock(void *buffer, int sector)
 {
-	fseek(fcdrom, 2048 * sector, SEEK_SET);
-	fread(buffer, 1, 2048, fcdrom);
+	fseek(fcdrom, (cd_sector_size * sector) + cd_sector_offset, SEEK_SET);
+	fread(buffer, 1, SECTOR_SIZE_2048, fcdrom);
 	rewind(fcdrom);
 	return 1;
 }
@@ -77,7 +147,7 @@ char *fsReadSize()
 	char *buffer = (char*)malloc(sizeof(char) * 4);
 
 	rewind(fcdrom);
-	fseek(fcdrom, 80, SEEK_SET);
+	fseek(fcdrom, 80 + cd_sector_offset, SEEK_SET);
 	fread(buffer, 1, 4, fcdrom);
 	rewind(fcdrom);
 	return buffer;
